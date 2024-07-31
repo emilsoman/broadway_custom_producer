1a15afd6a7e0361ea9a8015a140cb320466c0a66
gmnikhil <gm.nikhilofficial@gmail.com>
2024-07-31 13:50:38 +0530

    replicated db poll issue

--- lib/consumer_test/producer.ex
+++ lib/consumer_test/producer.ex
@@ -6,11 +6,10 @@ defmodule ConsumerTest.Producer do
   end
 
   def init(_) do
-    # state = get_report_items()
-    {:producer, %{items: [], pending_demand: 0}}
+    {:producer, %{items: [], pending_demand: 0, db_queue: Enum.to_list(1..17)}}
   end
 
-  def handle_demand(new_demand, %{items: items, pending_demand: pending_demand})
+  def handle_demand(new_demand, %{items: items, pending_demand: pending_demand} = state)
       when new_demand > 0 do
     demand = new_demand + pending_demand
     {to_send, rest} = Enum.split(items, demand)
@@ -22,18 +21,18 @@ defmodule ConsumerTest.Producer do
       Process.send_after(self(), :poll, 2000)
     end
 
-    {:noreply, to_send, %{items: rest, pending_demand: unfulfilled_demand}}
+    {:noreply, to_send, Map.merge(state, %{items: rest, pending_demand: unfulfilled_demand})}
   end
 
   def handle_info(:poll, state) do
-    case get_report_items() do
-      [] ->
+    case get_report_items(state) do
+      {[], state} ->
         # No new items to send, we will poll again
         # In prod, we can wait for atleast 5 minutes before polling again
         Process.send_after(self(), :poll, 2000)
         {:noreply, [], state}
 
-      items ->
+      {items, state} ->
         # We have new items to send.
         # We send the items and subtract from pending_demand.
         # The rest of the items are added to the state, so when the next handle_demand is called
@@ -45,18 +44,26 @@ defmodule ConsumerTest.Producer do
           Process.send_after(self(), :poll, 2000)
         end
 
-        {:noreply, to_send, %{items: rest, pending_demand: unfulfilled_demand}}
+        {:noreply, to_send, Map.merge(state, %{items: rest, pending_demand: unfulfilled_demand})}
     end
   end
 
-  defp get_report_items do
-    IO.inspect("Calling get_report_items #{inspect(DateTime.utc_now(:second))}")
-    # Most of the time there's nothing new to return,
-    # but sometimes we return a list of items
-    if Enum.random(1..10) > 8 do
-      Enum.to_list(1..30)
-    else
-      []
-    end
+  defp get_report_items(%{db_queue: db_queue} = state) do
+    items =
+      if Enum.empty?(db_queue) do
+        []
+      else
+        # in produze repo also we take a max of 10 items per db call
+        # it can be any number, but large enough to not make too many db calls
+        # and small enough to not block other nodes from getting data
+        # so as to maximize the parallelism of the system
+
+        Enum.take(db_queue, 10)
+      end
+
+    seconds = :second |> DateTime.utc_now() |> DateTime.to_unix() |> rem(100)
+    IO.inspect("Retrieved #{length(items)} items at #{inspect(seconds)}")
+
+    {items, %{state | db_queue: db_queue -- items}}
   end
 end
